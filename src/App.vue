2bc389ab84fe12d9d8e9a514b4bf492cdc90c364
KristapsLusis <kristapsl1803@gmail.com>
2019-11-14 13:26:10 +0100

    Routing & Header Components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from "./components/layout/Header.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
+</script>
 
 <style lang="scss">
 </style>

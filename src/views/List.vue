909ab00d92e2756884be052de9a6c13d8789af83
Casper Hauberg <casperhauberg@icloud.com>
2019-11-25 11:44:22 +0100

    list_intial_commit

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,14 +2,16 @@
   <div class="about">
     <Header />
     <h1>This is the list page</h1>
+    <List />
   </div>
 </template>
 
 <script>
 import Header from "../components/layout/Header.vue";
+import List from "../components/FilterableList";
 
 export default {
   name: "list",
-  components: { Header }
+  components: { Header, List }
 };
 </script>
